ab6dd04dcbfc0300e17772df9c8171557bf8fae0
Kovalev Ivan <j.j.cherdak@gmail.com>
2021-07-11 19:48:20 +0300

    Created an application Calculation. Layout is ready.

--- app/src/main/java/com/geekbrains/lesson_1/MainActivity.java
+++ app/src/main/java/com/geekbrains/lesson_1/MainActivity.java
@@ -3,8 +3,11 @@ package com.geekbrains.lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
